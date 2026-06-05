ad8cd6abbbdc8d7668bebf8de7359aae7c7668a8
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-06-05 23:13:16 +0530

    Updated voter program with if-else-if ladder

--- decision.c.c
+++ decision.c.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int age = 22; // Aapki umar 22 saal hai 
+    int age;
 
+    printf("Apni umar (Age) enter kijiye: ");
+    scanf("%d", &age); 
+
+    // Pehli Shart: Kya user 18 ya usse bada hai?
     if (age >= 18) {
-        printf("Aap vote de sakte hain! 🎉\n");
+        printf("Aap ek adult hain aur vote de sakte hain! \n");
     } 
+    // Doosri Shart: Agar pehli galat hai, toh kya user 13 ya usse bada hai?
+    else if (age >= 13) {
+        printf("Aap ek teenager hain. \n");
+    } 
+    // Aakhri Rasta: Agar upar ki saari shartein galat hain
     else {
-        printf("Aap abhi chote hain, vote nahi de sakte. ❌\n");
+        printf("Aap abhi bacche hain. \n");
     }
 
     return 0;
 }
-
