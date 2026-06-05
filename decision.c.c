486421fc4a658a179439df2a55bf0cf7aef2d18a
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-06-05 22:36:41 +0530

    Added C decision making if-else program

---
+++ decision.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 22; // Aapki umar 22 saal hai 
+
+    if (age >= 18) {
+        printf("Aap vote de sakte hain! 🎉\n");
+    } 
+    else {
+        printf("Aap abhi chote hain, vote nahi de sakte. ❌\n");
+    }
+
+    return 0;
+}
+
